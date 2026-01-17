961c73bfe1171b2fa201d71ca50e5ffb14a4af03
krikkirk <nuevodiia@gmail.com>
2026-01-16 23:52:43 -0500

    Update tagtype_db.cpp

--- tag_fw/hal/Newton_M3_nRF52811/tagtype_db.cpp
+++ tag_fw/hal/Newton_M3_nRF52811/tagtype_db.cpp
@@ -355,10 +355,10 @@ void identifyTagInfo() {
             tag.OEPLtype = SOLUM_M3_BWRY_30;
             epd->epdMirrorV = true;
             break;
-        case STYPE_SIZE_060_BWRY:
+        case STYPE_SIZE_60_BWRY:
             tag.macSuffix = 0x9690;
             epd->drawDirectionRight = false;
-            tag.OEPLtype = SOLUM_M3_BWRY_60;
+            tag.OEPLtype = SOLUM_M3_BWRY_060;
             epd->epdMirrorV = true;
             break;
         case STYPE_SIZE_013:
